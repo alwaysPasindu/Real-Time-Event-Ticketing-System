89fbfee50e8432bd91a3b4907c604b5ca36a4043
Pasindu Dilshan <always.pasindu@gmail.com>
2024-12-06 17:11:48 +0530

    Created the Ticket class implementations

--- TicketSystemCLI/src/Main/Ticket.java
+++ TicketSystemCLI/src/Main/Ticket.java
@@ -1,4 +1,20 @@
 package Main;
 
 public class Ticket {
+    private String ticketId;
+
+    // Constructor to create a ticket with a unique ID
+    public Ticket(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    // Getter for ticketId
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket ID: " + ticketId;
+    }
 }
