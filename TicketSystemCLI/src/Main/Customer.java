60c70c62b20aa7a4f9496037fda0c4110bce33d5
Pasindu Dilshan <always.pasindu@gmail.com>
2024-12-06 17:42:13 +0530

    Customer class implementations

--- TicketSystemCLI/src/Main/Customer.java
+++ TicketSystemCLI/src/Main/Customer.java
@@ -1,4 +1,24 @@
 package Main;
 
-public class Customer {
+public class Customer implements Runnable {
+    private final TicketPool ticketPool;
+    private final int purchaseInterval; // In milliseconds
+
+    public Customer(TicketPool ticketPool, int purchaseInterval) {
+        this.ticketPool = ticketPool;
+        this.purchaseInterval = purchaseInterval;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Ticket ticket = ticketPool.removeTicket(); // Try to purchase a ticket
+                System.out.println("Customer bought: " + ticket);
+                Thread.sleep(purchaseInterval); // Try to buy again after the specified interval
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Customer interrupted.");
+        }
+    }
 }
